d802f28f1222505ce99c54417f32f371c4f791a5
Alastair Lynn <arplynn@gmail.com>
2014-02-17 13:58:31 +0000

    Add Category, Arrow and ArrowTransformer instances

--- Control/Arrow/Improve.hs
+++ Control/Arrow/Improve.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 module Control.Arrow.Improve(ImproveArrow, lowerImprove) where
 
@@ -6,6 +8,8 @@ import Prelude hiding (id, (.))
 
 import Control.Category
 import Control.Arrow
+
+import Control.Arrow.Transformer
 
 data ImproveArrow a b c where
   IArrow :: a b c    -> ImproveArrow a b c
@@ -18,4 +22,33 @@ lowerImprove (IArrow a) = a
 lowerImprove (IArr f)   = arr f
 lowerImprove IId        = id
 lowerImprove (IConst k) = arr (\_ -> k)
+
+instance (Arrow a) => Category (ImproveArrow a) where
+  id = IId
+  IId      . x        = x
+  x        . IId      = x
+  IConst k . IArr _   = IConst k
+  IConst k . IConst _ = IConst k
+  IConst k . IArrow f = IArrow (f >>^ (\_ -> k))
+  IArr f   . IArr g   = IArr (f . g)
+  IArr f   . IConst k = IConst (f k)
+  IArr f   . IArrow g = IArrow (g >>^ f)
+  IArrow f . IArr g   = IArrow (g ^>> f)
+  IArrow f . IConst k = IArrow ((\_ -> k) ^>> f)
+  IArrow f . IArrow g = IArrow (g >>> f)
+
+instance (Arrow a) => Arrow (ImproveArrow a) where
+  arr = IArr
+  first (IArrow x) = IArrow (first x)
+  first (IArr f)   = IArr (first f)
+  first (IConst k) = IArr (first (\_ -> k))
+  first IId        = IId
+
+  second (IArrow x) = IArrow (second x)
+  second (IArr f)   = IArr (second f)
+  second (IConst k) = IArr (second (\_ -> k))
+  second IId        = IId
+
+instance (Arrow a) => ArrowTransformer ImproveArrow a where
+  lift = IArrow
 
